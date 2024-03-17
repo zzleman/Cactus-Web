cfd0f133f23d537f235ed4bf3a855824a7146d65
Leman <leman@Leman-MacBook-Air.local>
2024-03-17 14:25:43 +0400

    links are active

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,34 @@ const blurHeader = () =>{
                      : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+
+// Show scroll up
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive  = () => {
+  const scrollDown = window.scrollY 
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop -58,
+    sectionId = current.getAttribute('id'),
+    sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+  
+  if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+    sectionsClass.classList.add('active-link')
+  }else{
+    sectionsClass.classList.remove('active-link')
+  }
+  })
+}
